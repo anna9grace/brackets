97efa547990bb8cf14af38714d2ec265f0a1de72
anna9grace <anna9grace@gmail.com>
2021-03-15 16:00:50 +0300

    feat: solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brArr = str.split('');
+  const pairsCount = brArr.length / 2;
+  let brOpeningIndex = null;
+  const configOpeningBr = bracketsConfig.map((el) => el[0]);
+  const configClosingBr = bracketsConfig.map((el) => el[1]);
+   
+  if (brArr.length%2 !== 0) {
+    return false;
+  }
+  
+   const deletePairedBr = () => {
+        brOpeningIndex = brArr.findIndex((br, index) => {
+            let r = configOpeningBr.indexOf(br);
+            return r >= 0 && brArr[index + 1] === configClosingBr[r];
+        })
+        brArr.splice(brOpeningIndex, 2);
+        brOpeningIndex = null;
+   }
+
+   for (let i = 0; i < pairsCount; i++) {
+       deletePairedBr();
+   }
+
+  return brArr.length === 0;
 }
